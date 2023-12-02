14bc7015db54015d50aa9deb27d43eae5947c1f8
Nilesh0559 <nileshgajendragadkar@gmail.com>
2023-12-02 18:37:06 +0530

    Improved bubble Sort

--- DSA/src/BubbleSort.java
+++ DSA/src/BubbleSort.java
@@ -14,18 +14,38 @@ public class BubbleSort {
 	public static void main(String[] args) {
 //		Array containing elements
 		int[] arr = {2,7,1,9,3,5,8,4,6,0};
-
+		
+//		Print array
+		System.out.println("Sorted : " + Arrays.toString(arr));
+		
 //		Pass to bubble sort function
 		bubbleSort(arr);
+		
+		improvedBubbleSort(arr);
 		
 //		Print sorted array
 		System.out.println("Sorted : " + Arrays.toString(arr));
 
 	}
 
+	private static void improvedBubbleSort(int[] arr) {
+		for(int i = 0; i < arr.length-1; i++) {
+//			Below modification in condition will reduce the number of passes
+//			for already sorted data set
+			for(int j = 0; j < arr.length-1-i; j++) {
+//				Compare two adjacent elements and swap them if needed
+				if(arr[j] > arr[j+1])
+					swap(arr, j);
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		
+	}
+
 	private static void bubbleSort(int[] arr) {
 		for(int i = 0; i < arr.length-1; i++) {
 			for(int j = 0; j < arr.length-1; j++) {
+//				Compare two adjacent elements and swap them if needed
 				if(arr[j] > arr[j+1])
 					swap(arr, j);
 			}
