e641fb25c616e83e7e56305bea5c6df9892834b2
Nilesh0559 <nileshgajendragadkar@gmail.com>
2024-06-17 12:41:57 +0530

    uppercase, lowercase problem

--- Code/src/com/code/stream/Program.java
+++ Code/src/com/code/stream/Program.java
@@ -2,13 +2,30 @@ package com.code.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
 
 	/*
-	 * Write a Java program to calculate the average of a list of integers using streams.
+	 * Write a Java program to convert a list of strings to uppercase or lowercase using streams.
 	 */
 	public static void main(String[] args) {
+		List<String> colors = Arrays.asList("RED", "grEEn", "white", "Orange", "pink");
+		System.out.println(colors.toString());
+		List<String> upperCase = colors.stream()
+			.map(String::toUpperCase)
+			.collect(Collectors.toList());
+		System.out.println("Upper Case: " + upperCase.toString());
+		List<String> lowerCase = colors.stream()
+			.map(String::toLowerCase)
+			.collect(Collectors.toList());
+		System.out.println("Lower Case: " + lowerCase.toString());
+	}
+	
+	/*
+	 * Write a Java program to calculate the average of a list of integers using streams.
+	 */
+	public static void main1(String[] args) {
 		List<Integer> nums = Arrays.asList(1, 3, 6, 8, 10, 18, 36);
 		System.out.println(nums.toString());
 		double avg = nums.stream().mapToDouble(Integer::doubleValue)
