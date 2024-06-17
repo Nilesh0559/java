89965a4e9854d805c340a050afd2814b4b0e2786
Nilesh0559 <nileshgajendragadkar@gmail.com>
2024-06-17 12:40:35 +0530

    average problem

--- Code/src/com/code/stream/Program.java
+++ Code/src/com/code/stream/Program.java
@@ -1,5 +1,19 @@
 package com.code.stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Program {
 
+	/*
+	 * Write a Java program to calculate the average of a list of integers using streams.
+	 */
+	public static void main(String[] args) {
+		List<Integer> nums = Arrays.asList(1, 3, 6, 8, 10, 18, 36);
+		System.out.println(nums.toString());
+		double avg = nums.stream().mapToDouble(Integer::doubleValue)
+			.average()
+			.orElse(0.0);
+		System.out.println("Average: " + avg);
+	}
 }
